18c07dd7568fc4d5c449ea2eca50ba2030e05482
bhomma <bhomma.hong@gmail.com>
2024-09-05 17:48:54 +0700

    question1

---
+++ question1.dart
@@ -0,0 +1,11 @@
+main(){
+  String name = "John";
+  int age = 25;
+  double height = 175;
+  // print this message -> "ฉันชื่อ [name] ฉันอายุ [age] ปี และฉันสูง [height] เมตร")
+
+  // code here
+  print("ฉันชื่ิอ $name");
+   print("ฉันอายุ $age ปี");
+    print("และฉันสูง $height เมตร");
+}
