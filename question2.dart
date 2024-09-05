6ec923c399253c8f2c5a277b8e1f75b428d56b5a
bhomma <bhomma.hong@gmail.com>
2024-09-05 18:27:40 +0700

    question2

---
+++ question2.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var number = 1;
+  if (number > 0) {
+    print("number = ตัวเลขเป็นบวก");
+  }  else if (number < 0) {
+    print("number = ตัวเลขเป็นลบ");
+  } else {
+    print("number = ตัวเลขเป็นศูนย์");
+  }
+}
