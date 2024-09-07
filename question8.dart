792be35387209a452e8363a24d3aa12d35aa98f1
bhomma <bhomma.hong@gmail.com>
2024-09-07 15:30:06 +0700

    question8

--- question8.dart
+++ question8.dart
@@ -1,6 +1,7 @@
 void main(List<String> args) {
-  double radius = 7.5;
-  double circlearea = 3.14 * (radius * radius);
+  var radius = "7.5";
+  double r = double.parse(radius);
+  double circlearea = 3.14 * (r * r);
   print("รัศมีวงกลม = $radius");
   print("พื้นที่วงกลม = $circlearea");
 }
