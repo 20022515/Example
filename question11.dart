5edb4ad87a351f16916094bd30ad08db8d841ecb
bhomma <bhomma.hong@gmail.com>
2024-09-07 18:26:02 +0700

    question11

--- question11.dart
+++ question11.dart
@@ -1,8 +1,14 @@
 void main(List<String> args) {
   var number = "10";
   int n = int.parse(number);
-  for (int i = 1; i <= n; i++) {
-    print(
-        '$i'); // จะพิมพ์ข้อความซ้ำไป 10 รอบ โดยเพิ่มค่าของ (n) ครั้งละ1 โดยพิมพ์เริ่มจาก ค่าของ (i) = 1
+  print("จำนวนตัวเลขคือ 1 ถึง $n");
+  {
+    int sum = 0;
+    int i = 1;
+    do {
+      sum += i;
+      i++;
+    } while (i <= 10);
+    print("ผลรวมของตัวเลขจาก 1 ถึง 10 คือ: $sum");
   }
 }
