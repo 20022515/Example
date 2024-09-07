1a8f9a038eb44609b2a74f255efd963c38ea5e13
bhomma <bhomma.hong@gmail.com>
2024-09-07 12:55:35 +0700

    question5

---
+++ question5.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  double length = 10.0;
+  double width = 5.0;
+  double square;
+  square = length*width;
+  {
+    print ("ขนาดความยาว = $length เมตร");
+  }
+  {
+    print ("ขนาดความกว้าง = $width เมตร");
+  }
+  {
+    print("ขนาดพื้นที่สี่เหลี่ยม = $square ตารางเมตร");
+  }
+ }
