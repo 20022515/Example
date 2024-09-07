dae4c86ef0afb5db1f4ae7644f06575bf288299c
bhomma <bhomma.hong@gmail.com>
2024-09-07 17:29:57 +0700

    question11

---
+++ question11.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var number = "10";
+  int n = int.parse(number);
+  for (int i = 1; i <= n; i++) {
+    print(
+        '$i'); // จะพิมพ์ข้อความซ้ำไป 5 รอบ ตามค่าของ (n) โดยพิมพ์เริ่มจาก ค่าของ (i) = 1
+  }
+}
