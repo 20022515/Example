d1c016344b9f1836e40ff9cda5e69c16e758c216
bhomma <bhomma.hong@gmail.com>
2024-09-07 15:33:11 +0700

    question7

--- question7.dart
+++ question7.dart
@@ -1,8 +1,9 @@
 void main(List<String> args) {
-  int number = 7;
-  if (number % 2 < 1) {
+  var number = "8";
+  int n = int.parse(number);
+  if (n % 2 < 1) {
     print("เลขคู่");
-  } else if (number % 2 >= 1) {
+  } else if (n % 2 >= 1) {
     print("เลขคี่");
-  } 
+  }
 }
