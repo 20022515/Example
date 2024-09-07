f5fa346b4176c1de2757e24b73f1b6563c528fef
bhomma <bhomma.hong@gmail.com>
2024-09-07 15:38:43 +0700

    question6

--- question6.dart
+++ question6.dart
@@ -1,7 +1,8 @@
 void main(List<String> args) {
-int n = 5;
-for (int i = 1; i <= n; i++) {
-  print('$i'); // จะพิมพ์ข้อความซ้ำไป 5 รอบ ตามค่าของ (n) โดยพิมพ์เริ่มจาก ค่าของ (i) = 1
-}
+  var number = "10";
+  int n = int.parse(number);
+  for (int i = 1; i <= n; i++) {
+    print(
+        '$i'); // จะพิมพ์ข้อความซ้ำไป 5 รอบ ตามค่าของ (n) โดยพิมพ์เริ่มจาก ค่าของ (i) = 1
   }
-
+}
